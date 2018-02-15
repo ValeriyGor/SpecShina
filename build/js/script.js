c690ed8819ad62559c8cf3d88272909b4ed5e84e
ValeriyGor <valeriy.gorbachevskiy@gmail.com>
2018-02-15 02:53:39 +0200

    add mobile version

--- build/js/script.js
+++ build/js/script.js
@@ -1,4 +1,18 @@
+$( ".button-mobile svg" ).click(function() {
+	$(this).toggleClass("open");
+	$(".header__logo").toggleClass("open");
+	$(".header").toggleClass("opened");
+	$(".header__city").slideToggle(300);
+	$(".head-menu").slideToggle(300);
+	$(".header__search").slideToggle(300);
+});
 
-
-
-
+$( ".header__city p" ).click(function() {
+	$(this).toggleClass("open");
+	$(".header__city-list").slideToggle(300);
+	if ($(window).width() <= '767'){
+        $(".head-menu").slideToggle(300);
+		$(".header__search").slideToggle(300);;
+    }
+	
+});
